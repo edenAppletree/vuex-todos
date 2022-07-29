eabbdaa338b519c236a1c512293f5f5f4a47ebef
limengting <1443848358@qq.com>
2022-07-29 21:39:16 +0800

    合并button分支

---
+++ src/todos.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <button @click="changeSkin">换肤</button>
+    <div class="todoapp">
+      <todo-header></todo-header>
+      <todo-main></todo-main>
+      <todo-footer></todo-footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoFooter from './components/TodoFooter.vue' // shift + alt + 下
+import TodoHeader from './components/TodoHeader.vue' // shift + alt + 下
+import TodoMain from './components/TodoMain.vue' // shift + alt + 下
+import { mapActions } from 'vuex'
+export default {
+  data() {
+    return {
+      colors: [
+        'red',
+        'orange',
+        'yellow',
+        'green',
+        'cyan',
+        'blue',
+        'purple',
+        'white',
+        'black'
+      ],
+      skinColor: ''
+    }
+  },
+
+  components: {
+    TodoFooter,
+    TodoHeader,
+    TodoMain
+  },
+
+  created() {},
+
+  methods: {
+    ...mapActions(['skin/asyncChangeColor']),
+    changeSkin() {
+      this.skinColor =
+        this.colors[Math.floor(Math.random() * this.colors.length)]
+      console.log(this.skinColor)
+      this.$store.dispatch('skin/asyncChangeColor', this.skinColor)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
